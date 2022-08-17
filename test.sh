4a29f5dee7c7d39c11518bcc0707c2f418d2a9cb
Agamiru <chidinnaemekannadi@gmail.com>
2022-08-17 15:06:41 +0100

    changes

--- test.sh
+++ test.sh
@@ -1,2 +1,8 @@
-user=$USER
-echo $user
+#!/bin/bash
+
+pg_ami_id=$(curl https://keyvalue.immanuel.co/api/KeyVal/GetValue/xqzlp7yq/pg-ami-id)
+echo "PG AMI Id $pg_ami_id"
+aws cloudformation deploy \
+--stack-name Wallet-Postgres-Server \
+--template-file .circleci/cloudformation/postgres_w_monitoring.yml \
+--parameter-overrides StackID=test-stack,EnvironmentName=Wallet,PGImageID=$pg_ami_id
