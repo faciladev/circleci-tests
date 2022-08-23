a00c4c204e1838701e192825ab53a2a86965a76a
Agamiru <chidinnaemekannadi@gmail.com>
2022-08-23 10:10:28 +0100

    changes

--- test.sh
+++ test.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-pg_ami_id=$(curl https://keyvalue.immanuel.co/api/KeyVal/GetValue/xqzlp7yq/pg-ami-id)
-echo "PG AMI Id $pg_ami_id"
-aws cloudformation deploy \
---stack-name Wallet-Postgres-Server \
---template-file .circleci/cloudformation/postgres_w_monitoring.yml \
---parameter-overrides StackID=test-stack,EnvironmentName=Wallet,PGImageID=$pg_ami_id
+if [ `curl http://34.239.134.80 | grep "502 Bad Gateway"` ]
+then
+  echo "All is well"
+else
+  echo "All is not well"
+  exit 1
+fi
+
